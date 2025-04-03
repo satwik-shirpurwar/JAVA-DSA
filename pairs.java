9cfe7d769a261861642e94a7923392ff1789f8e8
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-04-03 19:22:37 +0530

    Add files via upload

---
+++ pairs.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class pairs {
+
+    public static void pair(int num[]){
+        for(int i=0;i<num.length;i++){
+            int curr=num[i];
+
+            for(int j=i+1;j<num.length;j++){
+                System.out.print("(" + curr +"," +num[j] +")");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+        pair(num);
+
+    }
+}
